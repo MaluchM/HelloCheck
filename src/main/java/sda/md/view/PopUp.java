05cad5d5bd86d7efdb29b2c96ec42aaf7c420f7a
iMac Magda <magda@ssolutions.com.pl>
2018-01-22 20:05:08 +0100

    add FX Aplications File

---
+++ src/main/java/sda/md/view/PopUp.java
@@ -0,0 +1,16 @@
+package sda.md.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PopUp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
